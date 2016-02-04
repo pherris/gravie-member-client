309862a827c9f3597493b76db7ec9b2613144a38
Andrew Ralston <aralston@gravie.com>
2016-02-04 15:26:38 -0600

    start building out page

--- project.clj
+++ project.clj
@@ -5,14 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.5.3"
-  
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"
                   :exclusions [org.clojure/tools.reader]]
                  [sablono "0.3.6"]
-                 [org.omcljs/om "0.9.0"]]
-  
+                 [org.omcljs/om "0.9.0"]
+                 [prismatic/om-tools "0.3.12"]]
+
   :plugins [[lein-figwheel "0.5.0-6"]
             [lein-cljsbuild "1.1.2" :exclusions [[org.clojure/clojure]]]]
 
@@ -45,7 +46,7 @@
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
-
+             :load-warninged-code true  ;; <- Add this
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
--- src/gravie_member_client/core.cljs
+++ src/gravie_member_client/core.cljs
@@ -1,27 +1,49 @@
-(ns gravie-member-client.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
+(ns ^:figwheel-always gravie-member-client.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]
+            [om-tools.dom :as dom :include-macros true]
             [clojure.string :as string]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(extend-type string
+  ICloneable
+  (-clone [s] (js/String. s)))
 
-;; define your app data so that it doesn't get over-written on reload
+(extend-type js/String
+  ICloneable
+  (-clone [s] (js/String. s))
+  om/IValue
+  (-value [s] (str s)))
 
-(defonce app-state
+(def app-state
   (atom
-    {:contacts
-     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+    {:people
+     [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker"
+       :email "aphacker@mit.edu"}
+      {:type :professor :first "Gerald" :middle "Jay" :last "Sussman"
+       :email "metacirc@mit.edu" :classes [:6001 :6946]}
+      {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu"
+       :classes [:6001]}]
+     :classes
+     {:6001 "The Structure and Interpretation of Computer Programs"
+      :6946 "The Structure and Interpretation of Classical Mechanics"
+      :1806 "Linear Algebra"}}))
 
-(defn middle-name [{:keys {middle middle-initial}}]
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn handle-change [e text owner]
+  (om/transact! text (fn [_] (.. e -target -value))))
+
+(defn commit-change [text owner]
+  (om/set-state! owner :editing false))
+
+(defn middle-name [{:keys [middle middle-initial]}]
   (cond
     middle (str " " middle)
     middle-initial (str " " middle-initial ".")))
@@ -29,62 +51,132 @@
 (defn display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
 
-(defn contact-view [contact owner]
-  (reify om/IRenderState
-    (render-state [this {:keys [delete]}]
+(defn student-view [student owner]
+  (reify
+    om/IRender
+    (render [_]
+            (dom/li nil (display-name student)))))
+
+(defn professor-view [professor owner]
+  (reify
+    om/IRender
+    (render [_]
             (dom/li nil
-                    (dom/span nil (display-name contact))
-                    (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
+                    (dom/div nil (display-name professor))
+                    (dom/label nil "Classes")
+                    (apply dom/ul nil
+                           (map #(dom/li nil (om/value %)) (:classes professor)))))))
 
-(defn parse-contact [contact-str]
-  (let [[first middle last :as parts] (string/split contact-str #"\s+")
-        [first last middle] (if (nil? last) [first middle] [first last middle])
-        middle (when middle (string/replace middle "." ""))
-        c (if middle (count middle) 0)]
-    (when (>= (count parts) 2)
-      (cond-> {:first first :last last}
-              (== c 1) (assoc :middle-initial middle)
-              (>= c 2) (assoc :middle middle)))))
+(defmulti entry-view (fn [person _] (:type person)))
 
-(defn add-contact [data owner]
-  (let [new-contact (-> (om/get-node owner "new-contact")
-                        .-value
-                        parse-contact)]
-    (when new-contact
-      (om/transact! data :contacts #(conj % new-contact)))))
+(defmethod entry-view :student
+  [person owner] (student-view person owner))
 
-(defn handle-change [e owner {:keys [text]}]
-  (om/set-state! owner :text (.. e -target -value)))
+(defmethod entry-view :professor
+  [person owner]
+  (professor-view person owner))
 
-(defn contacts-view [data owner]
+(defn people [data]
+  (->> data
+       :people
+       (mapv (fn [x]
+               (if (:classes x)
+                 (update-in x [:classes]
+                            (fn [cs] (mapv (:classes data) cs)))
+                 x)))))
+
+(defn registry-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "registry"}
+        (dom/h2 nil "Registry")
+               (apply dom/ul nil
+                      (om/build-all entry-view (people data)))))))
+
+(defn editable [text owner]
   (reify
     om/IInitState
     (init-state [_]
-                {:delete (chan)
-                 :text ""})
-    om/IWillMount
-    (will-mount [_]
-                (let [delete (om/get-state owner :delete)]
-                      (go (loop []
-                            (let [contact (<! delete)]
-                              (om/transact! data :contacts
-                                            (fn [xs] (vec (remove #(= contact %) xs))))
-                              (recur))))))
+      {:editing false})
     om/IRenderState
-      (render-state [this state]
-              (dom/div nil
-                       (dom/h2 nil "Contact List")
-                       (apply dom/ul nil
-                              (om/build-all contact-view (:contacts data)
-                                            {:init-state state}))
-                       (dom/div nil
-                               (dom/input #js {:type "text" :ref "new-contact"
-                                               :value (:text state)
-                                               :onChange #(handle-change % owner state)})
-                               (dom/button #js {:onClick #(add-contact data owner)} "Add Contact"))))))
+    (render-state [_ {:keys [editing]}]
+      (dom/li nil
+        (dom/span #js {:style (display (not editing))} (om/value text))
+        (dom/input
+          #js {:style (display editing)
+               :value (om/value text)
+               :onChange #(handle-change % text owner)
+               :onKeyDown #(when (= (.-key %) "Enter")
+                              (commit-change text owner))
+               :onBlur (fn [e] (commit-change text owner))})
+        (dom/button
+          #js {:style (display (not editing))
+               :onClick #(om/set-state! owner :editing true)}
+          "Edit")))))
 
-(om/root contacts-view app-state
-  {:target (. js/document (getElementById "contacts"))})
+(defn classes-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:id "classes"}
+        (dom/h2 nil "Classes")
+        (apply dom/ul nil
+               (om/build-all editable (vals (:classes data))))))))
+
+
+(defn glossary-term [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/a
+        #js {:className "query"}
+        data))))
+
+(defn form-field-required-icon [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/span {:className "has-error-icon glyphicon glyphicon-exclamation-sign"} ""))))
+
+(defn select-box [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (println (:options data))
+      (dom/select {:name "form-container" :className "form-control form-66 angular ng-pristine ng-valid ng-touched"}
+        (for [option (:options data)]
+          (dom/option option))))))
+
+(defn zip-and-county [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div
+        (dom/hr)))))
+
+(defn coverage-details [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div {:className "form-container"}
+        (dom/div {:className "form-group"}
+          (dom/label {:className "control-label col-sm-4"}
+            (om/build-all glossary-term ["Requested Start Date"])
+            (om/build-all form-field-required-icon [""])) ;;can I pass no arg to a component?
+          (dom/div {:className "col-sm-8"}
+            (om/build-all select-box [{:name "someName" :options ["Select..." "3/1/2016" "4/1/2014"]}])))
+        (om/build-all zip-and-county [""])))))
+
+
+(om/root registry-view app-state
+  {:target (. js/document (getElementById "registry"))})
+
+(om/root classes-view app-state
+  {:target (. js/document (getElementById "classes"))})
+
+(om/root coverage-details app-state
+         {:target (. js/document (getElementById "coverageDetails"))})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
