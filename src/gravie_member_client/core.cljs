b4107cd9759e29591185e56272e005817c6a9516
Andrew Ralston <aralston@gravie.com>
2016-02-05 09:41:34 -0600

    handle form states based on atom state

--- src/gravie_member_client/core.cljs
+++ src/gravie_member_client/core.cljs
@@ -10,9 +10,15 @@
     {:coverage-details {
                          :plan-coverage-date {
                                                :available-dates ["Select..." "3/1/2016" "4/1/2014" "5/1/2014"]
+                                               :selected "3/1/2016"
                                                :errors ["some error"]}
-                         :zip-code "55104"
-                         :counties ["Ramsey" "Hennepin"]
+                         :zip-code {
+                                     :selected "55104"
+                                     :errors nil}
+                         :county {
+                                   :selected "Ramsey"
+                                   :available-counties ["Ramsey" "Hennepin"]
+                                   :errors nil}
                          }
      :participants [{
                       :member true
@@ -38,12 +44,14 @@
       (dom/span {:className "has-error-icon glyphicon glyphicon-exclamation-sign"} ""))))
 
 (defn select-box [data owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/select {:name "form-container" :className "form-control form-66 angular ng-pristine ng-valid ng-touched"}
-        (for [option (:options data)]
-          (dom/option option))))))
+  (let [options (:options data)
+        selected (:selected data)]
+    (reify
+      om/IRender
+      (render [_]
+        (dom/select {:name "form-container" :className "form-control form-66 angular ng-pristine ng-valid ng-touched"}
+          (for [option options]
+            (dom/option (when (= selected option) {:selected true}) option)))))))
 
 (defn input-text [data owner]
   (reify
@@ -58,46 +66,55 @@
 (defn get-error [field app-state]
   (get-in app-state [field :errors]))
 
-(defn zip-and-county [data owner]
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div
-        (dom/hr)
-        (dom/div {:className "form-group"}
-          (dom/label {:className "control-label col-sm-4" :for "zipCode"}
-            (dom/span "ZIP Code")
-            (om/build form-field-required-icon ""))
-          (dom/div {:className "col-sm-8" }
-            (om/build input-text {
-                                   :id "zipCode"
-                                   :value (:zip-code (:coverage-details data))})))
-        (dom/div {:className "form-group"}
-            (dom/label {:className "control-label col-sm-4"}
-              (dom/span "County"))
-            (dom/div {:className "col-sm-8"}
-              (om/build select-box {:options ["Hennepin" "Ramsey"]})))
-        (dom/hr)))))
+(defn include-error-class [current-classes error-object]
+  (str current-classes " " (if error-object "has-error" "")))
 
-(defn coverage-details [data owner]
-  (reify
-    om/IRender
-    (render [_]
-            (println (get-error :plan-coverage-date (:coverage-details data)))
-      (dom/div {:className "form-container"}
-        (dom/div {:className "form-horizontal"}
-          (dom/div {:className (str
-                             "form-group "
-                             (if (get-error :plan-coverage-date (:coverage-details data)) "has-error" ""))}
-            (dom/label {:className "control-label col-sm-4"}
-              (om/build glossary-term ["Requested Start Date"])
-              (om/build form-field-required-icon [""])) ;;no-arg component?
-            (dom/div {:className "col-sm-8"}
-              (om/build select-box {
-                                     :name "planCoverageDate"
-                                     :options (get-in data [:coverage-details :plan-coverage-date :available-dates])})
-              (dom/span {:className "error-content"} (get-error :plan-coverage-date (:coverage-details data)))))
-          (om/build zip-and-county data))))))
+(defn zip-and-county [app-state owner]
+  (let [zip-code-error (get-error :zip-code (:coverage-details app-state))
+        county-error (get-error :county (:coverage-details app-state))
+        coverage-details (:coverage-details app-state)]
+    (reify
+      om/IRender
+      (render [_]
+        (dom/div
+          (dom/hr)
+          (dom/div {:className (include-error-class "form-group" zip-code-error)}
+            (dom/label {:className "control-label col-sm-4" :for "zipCode"}
+              (dom/span "ZIP Code")
+              (om/build form-field-required-icon ""))
+            (dom/div {:className "col-sm-8" }
+              (om/build input-text {
+                                     :id "zipCode"
+                                     :value (:selected (:zip-code coverage-details))})
+              (dom/span {:className "error-content"} zip-code-error)))
+          (dom/div {:className (include-error-class "form-group" county-error)}
+              (dom/label {:className "control-label col-sm-4"}
+                (dom/span "County"))
+              (dom/div {:className "col-sm-8"}
+                (om/build select-box {
+                                       :options (:available-counties (:county coverage-details))
+                                       :selected (:selected (:county coverage-details))})
+                (dom/span {:className "error-content"} county-error)))
+          (dom/hr))))))
+
+(defn coverage-details [app-state owner]
+  (let [plan-coverage-date-error (get-error :plan-coverage-date (:coverage-details app-state))]
+    (reify
+      om/IRender
+      (render [_]
+        (dom/div {:className "form-container"}
+          (dom/div {:className "form-horizontal"}
+            (dom/div {:className (include-error-class "form-group" plan-coverage-date-error)}
+              (dom/label {:className "control-label col-sm-4"}
+                (om/build glossary-term ["Requested Start Date"])
+                (om/build form-field-required-icon [""])) ;;no-arg component?
+              (dom/div {:className "col-sm-8"}
+                (om/build select-box {
+                                       :name "planCoverageDate"
+                                       :options (get-in app-state [:coverage-details :plan-coverage-date :available-dates])
+                                       :selected (get-in app-state [:coverage-details :plan-coverage-date :selected])})
+                (dom/span {:className "error-content"} plan-coverage-date-error)))
+            (om/build zip-and-county app-state)))))))
 
 (om/root coverage-details app-state
          {:target (. js/document (getElementById "coverageDetails"))})
